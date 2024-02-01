c27b12844a3ec326b885d1c455aff3e4a47f2a87
bretathompson <68876276+bretathompson@users.noreply.github.com>
2024-02-01 06:27:01 -0700

    Add files via upload

---
+++ model15-9-1/index.js
@@ -0,0 +1,24 @@
+
+// complete the following.
+
+// Open your command prompt and navigate to the folder that 'restaurants.json' is located in. Then run this command:
+// mongoimport --db otech-assignments --collection queryPractice --file restaurants.json --jsonArray
+
+// Your task for this assignment is to create a schema from the given data, and using mongoose, create the following queries:
+// 1. Get all of the documents in the collection
+// 2. Get the first 5 restaurants that are in the borough Bronx.
+// 3. Get all of the restaurants that scored more than 80, but less than 100
+// 4. Get the restaurant Id, Name, borough, and cuisine for all of the restaurants that contain 'Wil' as the first three letters in its name.
+// 5. Find all of the restaurants that are in latitude in the 70's range (you will need to use a range to include them all i.e. -70- to -79.9)
+// 6. Find all of the restaurants that are in zipcode 10014
+// 7. Get all of the restaurants that have a grades of 6 or more.
+// 8. Get the name and address of the restaurants that have a grade that is an 'A'.
+// 9. Find all of the restaurants that are in building 220.
+
+
+the file name given is a restaurants.json file
+
+
+
+
+
